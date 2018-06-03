bbc40bc6a3ad7fe85e8e16af90d4faaf60527fbd
sanadsa <sanad_s96@hotmail.com>
2018-06-03 18:49:45 +0300

    code first migration
    till seeding the database

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MemberShipType MemberShipType { get; set; }
+        public byte MemberShipTypeId { get; set; }
     }
 }

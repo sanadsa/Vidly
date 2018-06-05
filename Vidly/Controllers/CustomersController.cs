511ee37351a094226a4de5cfd51195c782940971
sanadsa <sanad_s96@hotmail.com>
2018-06-05 03:07:49 +0300

    Exersice 3.1
    Display membership type name

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,32 +10,36 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
-        {  
-            return View(getCustomers());
+        {
+            var customers = _context.Customers.Include(c => c.MemberShipType).ToList();
+
+            return View(customers);
         }
 
         //GET: Customers/Details/Id
         [Route("customers/details/{id}")]
         public ActionResult Details(int id)
         {           
-            var customer = getCustomers().Find(item => item.Id == id);
+            var customer = _context.Customers.SingleOrDefault(item => item.Id == id);
 
-            if (customer != null)
-            {
-                return View(customer);
-            }            
-            else
-            {
+            if (customer == null)
                 return HttpNotFound();
-            }
-        }
-
-        private List<Customer> getCustomers() => new List<Customer>
-            {
-                new Customer() { Id = 1, Name = "Jhon" },
-                new Customer() { Id = 2, Name = "mate" }
-            };
+                        
+            return View(customer);
+        }        
     }
 }
